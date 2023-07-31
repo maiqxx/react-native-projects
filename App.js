7b425ca75fa20bddcafd87a049d4f43325f12225
Jamaica Cavalida <jamaica.cavalida@ctu.edu.ph>
2023-07-31 09:01:47 +0800

    Modified the App.js and imported ProjectList

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  BackHandler,
+} from "react-native";
+import { Text, ListItem } from "react-native-elements";
+import Constants from "expo-constants";
+import { PROJECT_LIST } from './ProjectList'
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [projectIndex, setProjectIndex] = useState(null);
+
+    // Handle when user press Hardware Back Button
+    useEffect(() => {
+      const backAction = () => {
+        // Go back to Example List
+        if (projectIndex !== null) {
+          setProjectIndex(null);
+        } 
+        // Exit app if user currently in Example List
+        else {
+          BackHandler.exitApp();
+        }
+  
+        return true;
+      };
+  
+      // https://reactnative.dev/docs/backhandler
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        backAction
+      );
+  
+      return () => backHandler.remove();
+    }, [projectIndex]);
+  
+    if (projectIndex !== null) return PROJECT_LIST[projectIndex].component;
+
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text h4 style={styles.heading}>
+          React Native Expo Examples
+        </Text>
+  
+        <ScrollView>
+          {PROJECT_LIST.map((l, i) => (
+            <ListItem key={i} bottomDivider onPress={() => setProjectIndex(i)}>
+              <View>
+                <Text>Level {l.level}</Text>
+              </View>
+  
+              <ListItem.Content>
+                <ListItem.Title style={styles.title}>{l.name}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+  heading: {
+    textAlign: "center",
+    padding: 12,
+  },
+  title: {
+    fontWeight: "bold",
   },
 });
