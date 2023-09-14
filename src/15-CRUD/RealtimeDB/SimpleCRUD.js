f080ca5ca98aa585d1efd0486c6c0419a408af46
Jam C <83889170+maiqxx@users.noreply.github.com>
2023-09-14 19:23:06 +0800

    Modified ReadMe and CRUD implementation

--- src/15-CRUD/RealtimeDB/SimpleCRUD.js
+++ src/15-CRUD/RealtimeDB/SimpleCRUD.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'rea
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useState } from 'react'
-import { getDatabase, ref, set} from 'firebase/database';
+import { getDatabase, ref, set, update, onValue, remove} from 'firebase/database';
 import { db } from '../../../utils/config';
 
 const SimpleCRUD = () => {
@@ -10,24 +10,56 @@ const SimpleCRUD = () => {
   const [username, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  function create(){
-
+  //This function creates data and store in Firebase Realtime database
+  function createData(){
     set(ref(db, 'users/' + username),{
         username: username,
         email: email
     }).then(() =>{
         alert('Data submitted!');
-    }).catch((error) => {
 
+    }).catch((error) => {  
         alert('Error: ' + error.message);
-    })
 
+    })
   }
+
+  //Reads data: Enter the username, click the 'Read' button, then it will show the email of that user
+  function readData(){
+
+    const starCountRef = ref(db, 'users/' + username);
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+
+      setEmail(data.email);
+    });
+  }
+
+  //Updates the data in database
+  function updateData(){
+    update(ref(db, 'users/' + username),{
+        email: email
+    }).then(() =>{
+        alert('Data updated!');
+
+    }).catch((error) => {  
+        alert('Error: ' + error.message);
+
+    })
+  }
+
+  //Deletes data from the realtime database
+  function deleteData(){
+    remove(ref(db, 'users/' + username));
+    alert("User deleted!");
+  }
+
 
   return (
     <View style={styles.container}>
     <StatusBar style='auto' />
-      <Text>Simple CRUD</Text>
+      <Text style={styles.title}>Simple CRUD</Text>
+      <Text style={styles.subText}>A simple implementation of CRUD</Text>
 
         <TextInput 
         value={username}
@@ -41,11 +73,24 @@ const SimpleCRUD = () => {
         placeholder='Email' 
         style={styles.textBoxes}></TextInput>
 
-    <TouchableOpacity onPress={create}>
-        <Text>Submit</Text>
+    <TouchableOpacity onPress={createData} style={styles.buttonCreate}>
+        <Text>Create</Text>
+    </TouchableOpacity> 
+
+    <TouchableOpacity onPress={readData} style={styles.buttonRead}>
+        <Text>Read</Text>
+    </TouchableOpacity>
+    
+    <TouchableOpacity onPress={updateData} style={styles.buttonUpdate}>
+        <Text>Update</Text>
     </TouchableOpacity>
 
+    <TouchableOpacity onPress={deleteData} style={styles.buttonDelete}>
+        <Text>Delete</Text>
+    </TouchableOpacity>
     </View>
+
+    
   )
 }
 
@@ -58,6 +103,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         height: '100%',
     },
+    title:{
+        fontSize:20,
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+    subText:{
+        fontSize: 16,
+        marginBottom: 30,
+    },
     textBoxes:{
         width:'90%',
         fontSize: 18,
@@ -66,5 +120,38 @@ const styles = StyleSheet.create({
         borderWidth: 0.2,
         borderRadius: 10,
         marginBottom: 20,
-    }
+    },
+    buttonCreate:{
+        backgroundColor: '#1FDF64',
+        width: '90%',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    buttonRead:{
+        backgroundColor: '#026EC1',
+        width: '90%',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    buttonUpdate:{
+        backgroundColor: '#EE6C00',
+        width: '90%',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    buttonDelete:{
+        backgroundColor: '#FF0000',
+        width: '90%',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+
 })
